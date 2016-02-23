80b6557d8fb4b6a0312ae9e5f5e45cac9d1116cc
Sergejs Luhmirins <sergejs.luhmirins@gmail.com>
2016-02-23 19:37:18 +0200

    Day 8 done

--- src/days/week2/Day8.kt
+++ src/days/week2/Day8.kt
@@ -1,14 +1,34 @@
 package days.week2
 
 import core.AbstractDay
+import java.util.*
 
 class Day8 : AbstractDay("day_8") {
 
+    val CODE_PATTERN = "\\\\x[a-f0-9]{2}"
+
     override fun calculate(): String {
-        throw NotImplementedError()
+
+        var countCodeChars = input().map { it.length }.sum()
+        var countChars = input().map {
+            it.toLowerCase()
+                    .replace("\\\\", "X")
+                    .replace("\\\"", "X")
+                    .replace(Regex(CODE_PATTERN), "X")
+                    .length - 2 //2 for surrounding quotes
+        }.sum()
+        return (countCodeChars - countChars).toString()
     }
 
     override fun calculateAdvanced(): String {
-       throw NotImplementedError()
+
+        var initialCodeChars = input().map { it.length }.sum()
+        var countCodeChars = input().map {
+            it.toLowerCase()
+                    .replace("\\", "\\\\")
+                    .replace("\"", "\\\"")
+                    .length + 2 //2 for surrounding quotes
+        }.sum()
+        return (countCodeChars - initialCodeChars).toString()
     }
 }
