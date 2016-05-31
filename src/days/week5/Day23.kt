5f5a8f3cd991e14887b1f39abb116a6ec1f0ed13
Sergejs Luhmirins <sluhmirins@mobiquityinc.com>
2016-05-31 21:21:35 +0200

    Day 23 done

--- src/days/week5/Day23.kt
+++ src/days/week5/Day23.kt
@@ -1,14 +1,79 @@
 package days.week5
 
 import core.AbstractDay
+import core.extensions.isOdd
 
 class Day23 : AbstractDay("day_23") {
 
     override fun calculate(): String {
-        throw NotImplementedError()
+        val registers = mutableMapOf(
+                'a' to 0,
+                'b' to 0
+        )
+
+        input().map { it.toInstruction() }.executeInstructions(registers)
+
+        return registers['b'].toString()
     }
 
     override fun calculateAdvanced(): String {
-        throw NotImplementedError()
+        val registers = mutableMapOf(
+                'a' to 1,
+                'b' to 0
+        )
+
+        input().map { it.toInstruction() }.executeInstructions(registers)
+
+        return registers['b'].toString()
     }
+
+    private fun String.toInstruction(): Instruction = Instruction(tokens = this.split(' '));
+
+    private data class Instruction(var tokens: List<String>)
+
+    private fun Instruction.command(): String = this.tokens[0]
+    private fun Instruction.register(): Char = this.tokens[1].toCharArray()[0]
+    private fun Instruction.jumpValue(): Int = this.tokens[1].toInt()
+    private fun Instruction.conditionJumpValue(): Int = this.tokens[2].toInt()
+
+    private fun MutableMap<Char, Int>.valueOfRegister(instruction: Instruction): Int = this[instruction.register()] ?: 0
+
+
+    private fun List<Instruction>.executeInstructions(registers: MutableMap<Char, Int>) {
+        var instructionIndex = 0
+        while (instructionIndex < this.size) {
+            val instruction = this[instructionIndex]
+
+            when (instruction.command()) {
+                "hlf" -> {
+                    instructionIndex++
+                    registers[instruction.register()] = registers.valueOfRegister(instruction) / 2
+                }
+                "tpl" -> {
+                    instructionIndex++
+                    registers[instruction.register()] = registers.valueOfRegister(instruction) * 3
+                }
+                "inc" -> {
+                    instructionIndex++
+                    registers[instruction.register()] = registers.valueOfRegister(instruction) + 1
+                }
+                "jmp" -> {
+                    instructionIndex += instruction.jumpValue()
+                }
+                "jie" -> {
+                    if (registers.valueOfRegister(instruction).isOdd())
+                        instructionIndex++
+                    else
+                        instructionIndex += instruction.conditionJumpValue()
+                }
+                "jio" -> {
+                    if (registers.valueOfRegister(instruction) == 1)
+                        instructionIndex += instruction.conditionJumpValue()
+                    else
+                        instructionIndex++
+                }
+            }
+        }
+    }
+
 }
--- test/days/week5/TestDay23.kt
+++ test/days/week5/TestDay23.kt
@@ -7,8 +7,8 @@ import org.junit.Test
  */
 class TestDay23 {
 
-    val SIMPLE_ANSWER = "0"
-    val ADVANCED_ANSWER = "0"
+    val SIMPLE_ANSWER = "255"
+    val ADVANCED_ANSWER = "334"
 
     @Test
     fun testCalculate() {
